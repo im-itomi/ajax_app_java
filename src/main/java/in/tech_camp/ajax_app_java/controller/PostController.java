ec13bf7094bbd17459e14d2fb19868019df79e49
m-ito <minami.ito@infomart.co.jp>
2025-01-24 15:31:05 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,12 +32,24 @@ public class PostController {
   //     return "posts/postForm";
   // }
 
+  // 同期通信でHTMLファイルを返却する場合の処理
+  // @PostMapping("/posts")
+  // public String savePost(@ModelAttribute("postForm") PostForm form){
+  //   PostEntity post = new PostEntity();
+  //   post.setContent(form.getContent());
+  //   postRepository.insert(post);
+  //   return "redirect:/";
+  // }
+
+  // 非同期通信
   @PostMapping("/posts")
-  public String savePost(@ModelAttribute("postForm") PostForm form){
+  public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form){
     PostEntity post = new PostEntity();
     post.setContent(form.getContent());
     postRepository.insert(post);
-    return "redirect:/";
+    // 作成したデータをEntityに保存
+    PostEntity resultPost = postRepository.findById(post.getId());
+    return ResponseEntity.ok(resultPost);
   }
   
 }
